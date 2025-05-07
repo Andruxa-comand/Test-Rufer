6b517542febd462df6e8e4c00e5726d77cf25ade
Andruxa-comand <tiurin.aa2009@gmail.com>
2025-05-07 18:27:18 +0300

    privet_v1

---
+++ my_app.py
@@ -0,0 +1,28 @@
+from instr import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.button = QPushButton(txt_next)
+        self.layout = QVBoxLayout()
+
+app = QApplication([])
+main_win = QWidget()
+main_win.show()
+app.exec_()
+
+
+
+
